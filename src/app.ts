6bed07807aa54a2693d5efc6ed876d97e2ae4a43
carlosdust.dev@gmail.com <carlosdust.dev@gmail.com>
2025-08-14 18:28:03 -0300

    refatora a configuração do app para incluir controle de produtos e reorganiza a estrutura de registro das rotas

--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,36 @@
 import Fastify from "fastify"
 import { loggerMiddleware } from "./middlewares/logger"
+
 import { InMemoryOrderRepository } from "./repositories/inMemoryOrderRepository"
 import { OrderService } from "./services/orderService"
 import { OrderController } from "./controllers/orderController"
 import { orderRoutes } from "./routes/orderRoutes"
+
+import { ProductController } from "./controllers/productController"
+import { productRoutes } from "./routes/productsRoutes"
 
 export function buildApp() {
   const app = Fastify({
     logger: false,
   })
 
-
   app.register(loggerMiddleware)
 
-  const repo = new InMemoryOrderRepository()
-  const service = new OrderService(repo)
-  const controller = new OrderController(service)
-
+  // Orders
+  const orderRepo = new InMemoryOrderRepository()
+  const orderService = new OrderService(orderRepo)
+  const orderController = new OrderController(orderService)
   app.register(async (instance) => {
-    await orderRoutes(instance, controller)
+    await orderRoutes(instance, orderController)
   })
 
+  // Products
+  const productController = new ProductController()
+  app.register(async (instance) => {
+    await productRoutes(instance, productController)
+  })
+
+  // Health check
   app.get("/health", async () => ({ status: "ok" }))
 
   return app
